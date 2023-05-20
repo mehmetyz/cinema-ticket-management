6f144dcca6adf71e5751c95a16bb6023f9bf68b8
mehmetyz <mehmetyildiz497@icloud.com>
2023-05-20 18:21:29 +0300

    fix coupon constructor problem

--- backend/src/main/java/com/sqlcinema/backend/model/Coupon.java
+++ backend/src/main/java/com/sqlcinema/backend/model/Coupon.java
@@ -1,11 +1,13 @@
 package com.sqlcinema.backend.model;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 import java.util.Date;
 
 @Data
 @SuperBuilder
+@NoArgsConstructor
 public class Coupon {
     private String code;
     private int couponLeft;
